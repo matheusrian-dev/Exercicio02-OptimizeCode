831297d3275d58e9822e31888856611a69b82810
x14081950 <x14081950@D28G82200716339>
2023-01-18 12:18:50 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio02-OptimizeCode/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Teste
+{
+    // Main Method
+    static public void Main(String[] args)
+    {
+        Console.WriteLine(Exercise.ReplaceDigits("4 score and 7 years ago, 8 men had the same PIN code: 6571"));
+    }
+}
+
+public class Exercise
+{
+    public static string ReplaceDigits(string sentence)
+    {
+        string[] words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" }; //array de strings com as palavras para substituir
+        for (int i = 0; i < sentence.Length; i++) //percorre a sentence
+        {
+            for (int j = 0; j < words.Length; j++) //percorre o array para verificar se há um dígito na posição atual
+            {
+                if (sentence[i].Equals(j.ToString()[0])) //se o valor for igual, substitui o dígito pela string
+                {
+                    sentence = sentence.Replace(sentence[i].ToString(), words[j] + " "); //adiciona também um espaço em branco para separar palavras.
+                }
+            }
+        }
+        RegexOptions options = RegexOptions.None; //configuração do regex
+        Regex regex = new Regex("[ ]{2,}", options); //inicialização do regex
+        sentence = regex.Replace(sentence, " "); //remove espaços em branco duplicados
+        sentence = sentence.Trim();//remove espaços em branco no início e final da string
+        return sentence;
+    }
+}
---
+++ Exercicio02-OptimizeCode/README.md
@@ -0,0 +1,10 @@
+﻿Exercício 02 - Substitua Dígitos por Palavras em uma String
+
+Escreva o método ReplaceDigits na classe Exercise que receba uma string chamada 'sentence' e faça o seguinte:
+
+Para cada ocorrência de digitos '0', '1','2', '3', '4', '5', '6', '7', '8' e '9', substitua-os pelas palavras 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', eight', ou 'nine' respectivamente. Retorne a string sentence modificada com todos os digitudos substituídos e insira espaços apropriados.
+
+Exemplo:
+
+Input: "4 score and 7 years ago, 8 men had the same PIN code: 6571"
+Output: "four score and seven years ago, eight men had the same PIN code: six five seven one"
